6e9d4677d956391c7c7adc5b5c696298556dab0a
unknown <viktall007@gmail.com>
2024-09-23 23:18:57 +0100

    prep2 updated

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name ? : ");
-        string firstname = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name ? : ");
-        string lastname = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
-        Console.Write($"Your name is {lastname}, {firstname} {lastname}");
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        string letter = "";
+        Console.Write("What percentage did you score? : ");
+        string score = Console.ReadLine();
+
+        int percentage = int.Parse(score);
+        
+        if(percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if(percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if(percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if(percentage >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+
+        Console.WriteLine($"You scored a/an {letter}");
+        
+        if(percentage >= 70)
+        {
+            Console.Write("Congratulation! you passed the course");
+        }
+
+        else
+        {
+            Console.Write("Oouchh! You failed. You can do better next time");
+        }
     }
 }
