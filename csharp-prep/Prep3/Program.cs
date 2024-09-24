af888c86497c275344ca5a996bb9d1ebe01e7150
unknown <viktall007@gmail.com>
2024-09-24 02:14:11 +0100

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int count = 0;
+        int guessNum = -1;
+
+        Console.Write("What is your guess? : ");
+        
+        while (guessNum != number)
+        {
+
+            guessNum = int.Parse(Console.ReadLine());
+
+            if (guessNum < number)
+            {
+                Console.Write("Guess higher : ");
+            }
+
+            else if(guessNum > number)
+            {
+                Console.Write("Guess lower : ");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed correctly");
+            }
+
+            count += 1;
+        }
+          Console.WriteLine($"You made {count} guesses");  
+
+        //   Console.Write("Would you like to try again? : ");
+        //   string response = Console.ReadLine();
+
+        //   if (response == "yes")
+        //   {
+        
+        //   }
     }
+
+        
 }
